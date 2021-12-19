e048035640c945e2abca07734e96af398761544c
Mariam <mariamz.dev@gmail.com>
2021-12-19 20:04:18 +0100

    comeback

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
+/* Blur on scroll */
+
 $(window).scroll(function(){
     var scroll = $(window).scrollTop();
     $('.section-one').css({
-        filter: "blur(" + (scroll/60) + "px)"
+        filter: "blur(" + (scroll/100) + "px)"
     })
 })
+
+/* Scroll to activities */
 
 $("button").click(function() {
     $('html,body').animate({
         scrollTop: $(".second-section").offset().top},
         'slow');
 });
+
+/* change icon */
+
+let img = document.querySelector('img');
+    let start = img.src;
+    let hover = img.getAttribute('data-hover');
+
+    img.onmouseover = () => { img.src = hover; }
+    img.onmouseout = () => { img.src = start; }
