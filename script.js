7e693221589aebb372a50fd24ba47cca8e1f646f
Mariam <mariamz.dev@gmail.com>
2021-12-20 11:47:06 +0100

    updated node modules

--- script.js
+++ script.js
@@ -23,3 +23,17 @@ let img = document.querySelector('img');
 
     img.onmouseover = () => { img.src = hover; }
     img.onmouseout = () => { img.src = start; }
+
+/*     const guild = bot.guilds.get('815622762416308274');
+    const totalUsers = bot.channels.get('853404895943327744');
+    console.log(totalUsers); */
+/* Guild ID: 853404895943327744 */
+/* application token: NzcxNDA2NDExNjk5NTg1MDM1.X5rqNA.tTOM2UVc33Q_PJwFJz3KMvR2fPU */
+
+import { Client } from "discord.js";
+const token = "NzcxNDA2NDExNjk5NTg1MDM1.X5rqNA.tTOM2UVc33Q_PJwFJz3KMvR2fPU"
+
+const guildID = "764495726909587476"
+discord.bot = new Client();
+var memberCount = discord.bot.guilds.get(guildID).members.size()
+console.log("There are " + memberCount + " people in this server!")
